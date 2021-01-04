1429404168f32507c770f15c7103a144e73c6b39
Prasana Devi Palanisamy <prasanadevip@gmail.com>
2021-01-04 17:32:37 -0600

    Palindrome project added

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -8,8 +8,28 @@ namespace Palindrome
 {
     class Program
     {
+        // Palindrome means A word, phrase, or sequence that reads the same backward as forward, e.g., madam 
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter the string to check if it is a palindrome or not: ");
+            string input = Console.ReadLine();
+            string reverse = string.Empty;
+            int length = input.Length - 1;
+
+            while(length >=0)
+            {
+                reverse = reverse + input[length];
+                length--;
+            }
+
+            if(input == reverse)
+            {
+                Console.WriteLine("The entered string is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The entered string is not a palindrome");
+            }
             Console.ReadLine();
         }
     }
